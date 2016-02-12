79a32f272e85b055a1e37ad5b1f22b4beb9e8d37
asmalik107 <asmalik@outlook.com>
2016-02-12 17:58:17 +0000

    Add css loader and webpack-merge

--- app/main.js
+++ app/main.js
@@ -1,3 +1,7 @@
+
+require('./css/main.css');
+
+
 import Hello from './hello/hello.jsx';
 import World from './hello/world.jsx';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 var path = require('path');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
-module.exports = {
-    entry: './app/main.js',
-    output: { path: __dirname +'/build', filename: 'bundle.js' },
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
+
+
+/*
+ module.exports = {
+ entry: './app/main.js',
+ output: {path: __dirname + '/build', filename: 'bundle.js'},
+ module: {
+ loaders: [
+ {
+ test: /.jsx?$/,
+ loader: 'babel-loader',
+ exclude: /node_modules/,
+ query: {
+ presets: ['es2015', 'react']
+ }
+ }
+ ]
+ },
+ };*/
+
+
+const common = {
+    entry: PATHS.app + '/main.js',
+    output: {path: PATHS.build, filename: 'bundle.js'},
     module: {
         loaders: [
             {
@@ -13,7 +41,25 @@ module.exports = {
                 query: {
                     presets: ['es2015', 'react']
                 }
+            },
+            {
+                // Test expects a RegExp! Note the slashes!
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                // Include accepts either a path or an array of paths.
+                include: PATHS.app
             }
         ]
-    },
+    }
 };
+
+
+if (TARGET === 'dev-server' || !TARGET) {
+    module.exports = merge(common, {
+        devtool: 'eval-source-map'
+    });
+}
+
+if (TARGET === 'build') {
+    module.exports = merge(common, {});
+}
